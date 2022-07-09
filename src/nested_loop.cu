654693a63aee4cb16517dde68ca12060ecd07f6b
Bruno Golosio <golosio@unica.it>
2022-07-09 05:36:28 +0200

    Implemented different nested loop algorithms

--- src/nested_loop.cu
+++ src/nested_loop.cu
@@ -27,6 +27,54 @@
 #include "utilities.h"
 #include "nested_loop.h"
 
+const int Ny_arr_size_ = 24;
+int Ny_th_arr_[] = {
+  355375,
+  215546,
+  48095,
+  29171,
+  29171,
+  10731,
+  10731,
+  17693,
+  10731,
+  6509,
+  6509,
+  3948,
+  2395,
+  1452,
+  881,
+  534,
+  324,
+  197,
+  119,
+  119,
+  119,
+  72,
+  72,
+  72
+};
+
+namespace NestedLoop
+{
+  //#include "Ny_th.h"
+  void *d_sort_storage_;
+  size_t sort_storage_bytes_;
+  void *d_reduce_storage_;
+  size_t reduce_storage_bytes_;
+
+  int Nx_max_;
+  int *d_max_Ny_;
+  int *d_sorted_Ny_;
+
+  int *d_idx_;
+  int *d_sorted_idx_;
+
+  int block_dim_x_;
+  int block_dim_y_;
+  int frame_area_;
+  float x_lim_;
+}
 
 //TMP
 #include "getRealTime.h"
@@ -52,10 +100,55 @@ namespace NestedLoop
 //////////////////////////////////////////////////////////////////////
 int NestedLoop::Init()
 {
+  //return Init(65536*1024);
+  return Init(128*1024);
+}
+
+//////////////////////////////////////////////////////////////////////
+int NestedLoop::Init(int Nx_max)
+{
   //prefix_scan_.Init();
   gpuErrchk(cudaMalloc(&d_Ny_cumul_sum_,
 			  PrefixScan::AllocSize*sizeof(int)));
-  
+
+  if (Nx_max <= 0) return 0;
+
+  block_dim_x_ = 32;
+  block_dim_y_ = 32;
+  frame_area_ = 65536*64;
+  x_lim_ = 0.75;
+  Nx_max_ = Nx_max;
+
+  gpuErrchk(cudaMalloc(&d_max_Ny_, sizeof(int)));  
+  gpuErrchk(cudaMalloc(&d_sorted_Ny_, Nx_max*sizeof(int)));
+  gpuErrchk(cudaMalloc(&d_idx_, Nx_max*sizeof(int)));
+  gpuErrchk(cudaMalloc(&d_sorted_idx_, Nx_max*sizeof(int)));
+
+  int *h_idx = new int[Nx_max];
+  for(int i=0; i<Nx_max; i++) {
+    h_idx[i] = i;
+  }  
+  gpuErrchk(cudaMemcpy(d_idx_, h_idx, Nx_max*sizeof(int),
+			  cudaMemcpyHostToDevice));
+  delete[] h_idx;
+    
+  // Determine temporary storage requirements for RadixSort
+  d_sort_storage_ = NULL;
+  sort_storage_bytes_ = 0;
+  cub::DeviceRadixSort::SortPairs(d_sort_storage_, sort_storage_bytes_,
+				  d_sorted_Ny_, d_sorted_Ny_, d_idx_,
+				  d_sorted_idx_, Nx_max);
+  // Determine temporary device storage requirements for Reduce
+  d_reduce_storage_ = NULL;
+  reduce_storage_bytes_ = 0;
+  int *d_Ny = NULL;
+  cub::DeviceReduce::Max(d_reduce_storage_, reduce_storage_bytes_, d_Ny,
+			 d_max_Ny_, Nx_max);
+
+  // Allocate temporary storage
+  gpuErrchk(cudaMalloc(&d_sort_storage_, sort_storage_bytes_));
+  gpuErrchk(cudaMalloc(&d_reduce_storage_, reduce_storage_bytes_));
+
   return 0;
 }
 
--- src/nested_loop.h
+++ src/nested_loop.h
@@ -18,10 +18,50 @@
  *
  */
 
+#include <cub/cub.cuh>
+#include "cuda_error.h"
 #include "get_spike.h"
 #include "rev_spike.h"
 
+extern const int Ny_arr_size_;
+extern int Ny_th_arr_[];
+
+namespace NestedLoop
+{
+  //#include "Ny_th.h"
+  extern void *d_sort_storage_;
+  extern size_t sort_storage_bytes_;
+  extern void *d_reduce_storage_;
+  extern size_t reduce_storage_bytes_;
+
+  extern int Nx_max_;
+  extern int *d_max_Ny_;
+  extern int *d_sorted_Ny_;
+
+  extern int *d_idx_;
+  extern int *d_sorted_idx_;
+
+  extern int block_dim_x_;
+  extern int block_dim_y_;
+  extern int frame_area_;
+  extern float x_lim_;
+}
+
+
 extern int *d_Ny_cumul_sum_;
+
+//////////////////////////////////////////////////////////////////////
+template<int i_func> 
+__global__ void BlockStepNestedLoopKernel(int Nx, int *Ny)
+{
+ const int ix = blockIdx.x;
+  if (ix < Nx) {
+    const int ny = Ny[ix];
+    for (int iy = threadIdx.x; iy < ny; iy += blockDim.x){
+      NestedLoopFunction<i_func>(ix, iy);
+    }
+  }
+}
 
 template<int i_func> 
 __global__ void CumulSumNestedLoopKernel(int Nx, int *Ny_cumul_sum,
@@ -36,6 +76,98 @@ __global__ void CumulSumNestedLoopKernel(int Nx, int *Ny_cumul_sum,
   }
 }
 
+//////////////////////////////////////////////////////////////////////
+template<int i_func> 
+__global__ void SimpleNestedLoopKernel(int Nx, int *Ny)
+{
+  int ix = (blockIdx.x * blockDim.x) + threadIdx.x;
+  int iy = (blockIdx.y * blockDim.y) + threadIdx.y;
+  if (ix<Nx && iy<Ny[ix]) {
+    NestedLoopFunction<i_func>(ix, iy);
+  }
+}
+
+//////////////////////////////////////////////////////////////////////
+template<int i_func> 
+__global__ void  ParallelInnerNestedLoopKernel(int ix, int Ny)
+{
+  int iy = threadIdx.x + blockIdx.x * blockDim.x;
+  if (iy<Ny) {
+    NestedLoopFunction<i_func>(ix, iy);
+  }
+}
+
+//////////////////////////////////////////////////////////////////////
+template<int i_func> 
+__global__ void  ParallelOuterNestedLoopKernel(int Nx, int *d_Ny)
+{
+  int ix = threadIdx.x + blockIdx.x * blockDim.x;
+  if (ix<Nx) {
+    for (int iy=0; iy<d_Ny[ix]; iy++) {
+      NestedLoopFunction<i_func>(ix, iy);
+    }
+  }
+}
+
+
+//////////////////////////////////////////////////////////////////////
+template<int i_func> 
+__global__ void Frame1DNestedLoopKernel(int ix0, int dim_x, int dim_y,
+					int *sorted_idx, int *sorted_Ny)
+{
+  int array_idx = threadIdx.x + blockIdx.x * blockDim.x;
+  if (array_idx<dim_x*dim_y) {
+    int ix = ix0 + array_idx % dim_x;
+    int iy = array_idx / dim_x;
+    if (iy<sorted_Ny[ix]) {
+      // call here the function that should be called by the nested loop
+      NestedLoopFunction<i_func>(sorted_idx[ix], iy);
+    }
+  }
+}
+
+//////////////////////////////////////////////////////////////////////
+template<int i_func> 
+__global__ void Frame2DNestedLoopKernel(int ix0, int dim_x, int dim_y,
+					int *sorted_idx, int *sorted_Ny)
+{
+  int ix = (blockIdx.x * blockDim.x) + threadIdx.x;
+  int iy = (blockIdx.y * blockDim.y) + threadIdx.y;
+  if (ix<dim_x && iy<sorted_Ny[ix+ix0]) {
+    // call here the function that should be called by the nested loop
+    NestedLoopFunction<i_func>(sorted_idx[ix+ix0], iy);
+  }
+}
+
+//////////////////////////////////////////////////////////////////////
+template<int i_func> 
+__global__ void Smart1DNestedLoopKernel(int ix0, int iy0, int dim_x, int dim_y,
+                                 int *sorted_idx, int *sorted_Ny)
+{
+  int array_idx = threadIdx.x + blockIdx.x * blockDim.x;
+  if (array_idx<dim_x*dim_y) {
+    int ix = ix0 + array_idx % dim_x;
+    int iy = iy0 + array_idx / dim_x;
+    if (iy<sorted_Ny[ix]) {
+      // call here the function that should be called by the nested loop
+      NestedLoopFunction<i_func>(sorted_idx[ix], iy);
+    }
+  }
+}
+
+//////////////////////////////////////////////////////////////////////
+template<int i_func> 
+__global__ void Smart2DNestedLoopKernel(int ix0, int iy0, int dim_x,
+					int dim_y, int *sorted_idx,
+					int *sorted_Ny)
+{
+  int ix = (blockIdx.x * blockDim.x) + threadIdx.x;
+  int iy = iy0 + (blockIdx.y * blockDim.y) + threadIdx.y;
+  if (ix<dim_x && iy<sorted_Ny[ix+ix0]) {
+    // call here the function that should be called by the nested loop
+    NestedLoopFunction<i_func>(sorted_idx[ix+ix0], iy);
+  }
+}
 
 
 
@@ -49,12 +181,41 @@ namespace NestedLoop
   extern PrefixScan prefix_scan_;
   
   int Init();
-  
+
+  int Init(int Nx_max);
+
   template<int i_func>
     int Run(int Nx, int *d_Ny);
 
   template<int i_func>
+  int BlockStepNestedLoop(int Nx, int *d_Ny);
+  
+  template<int i_func>
   int CumulSumNestedLoop(int Nx, int *d_Ny);  
+
+  template<int i_func>
+  int SimpleNestedLoop(int Nx, int *d_Ny);
+
+  template<int i_func>
+  int SimpleNestedLoop(int Nx, int *d_Ny, int max_Ny);
+
+  template<int i_func>
+  int ParallelInnerNestedLoop(int Nx, int *d_Ny);
+
+  template<int i_func>
+  int ParallelOuterNestedLoop(int Nx, int *d_Ny);
+
+  template<int i_func>
+  int Frame1DNestedLoop(int Nx, int *d_Ny);
+
+  template<int i_func>
+  int Frame2DNestedLoop(int Nx, int *d_Ny);
+
+  template<int i_func>
+  int Smart1DNestedLoop(int Nx, int *d_Ny);
+
+  template<int i_func>
+  int Smart2DNestedLoop(int Nx, int *d_Ny);
 
   int Free();
 }
@@ -63,9 +224,312 @@ namespace NestedLoop
 template<int i_func>
 int NestedLoop::Run(int Nx, int *d_Ny)
 {
+  //return BlockStepNestedLoop<i_func>(Nx, d_Ny);
   return CumulSumNestedLoop<i_func>(Nx, d_Ny);
+  //return SimpleNestedLoop<i_func>(Nx, d_Ny);
+  //return ParallelInnerNestedLoop<i_func>(Nx, d_Ny);
+  //return ParallelOuterNestedLoop<i_func>(Nx, d_Ny);
+  //return Frame1DNestedLoop<i_func>(Nx, d_Ny);
+  //return Frame2DNestedLoop<i_func>(Nx, d_Ny);
+  //return Smart1DNestedLoop<i_func>(Nx, d_Ny);
+  //return Smart2DNestedLoop<i_func>(Nx, d_Ny);  
 }
 
+
+//////////////////////////////////////////////////////////////////////
+template<int i_func>
+int NestedLoop::BlockStepNestedLoop(int Nx, int *d_Ny)
+{
+  BlockStepNestedLoopKernel<i_func><<<Nx, 1024>>>(Nx, d_Ny);
+  gpuErrchk(cudaPeekAtLastError());
+  gpuErrchk(cudaDeviceSynchronize());
+  
+  return 0;
+}
+
+//////////////////////////////////////////////////////////////////////
+template<int i_func>
+int NestedLoop::SimpleNestedLoop(int Nx, int *d_Ny)
+{
+  // Find max value of Ny
+  cub::DeviceReduce::Max(d_reduce_storage_, reduce_storage_bytes_, d_Ny,
+			 d_max_Ny_, Nx);
+  int max_Ny;
+  gpuErrchk(cudaMemcpy(&max_Ny, d_max_Ny_, sizeof(int),
+			  cudaMemcpyDeviceToHost));
+  return SimpleNestedLoop<i_func>(Nx, d_Ny, max_Ny);
+}
+
+//////////////////////////////////////////////////////////////////////
+template<int i_func>
+int NestedLoop::SimpleNestedLoop(int Nx, int *d_Ny, int max_Ny)
+{
+  if (max_Ny < 1) max_Ny = 1;
+  dim3 threadsPerBlock(block_dim_x_, block_dim_y_);  // block size
+  dim3 numBlocks((Nx - 1)/threadsPerBlock.x + 1,
+		 (max_Ny - 1)/threadsPerBlock.y + 1);
+  SimpleNestedLoopKernel<i_func> <<<numBlocks,threadsPerBlock>>>(Nx, d_Ny);
+  gpuErrchk(cudaPeekAtLastError());
+  gpuErrchk(cudaDeviceSynchronize());
+
+  return 0;
+}
+
+//////////////////////////////////////////////////////////////////////
+template<int i_func>
+int NestedLoop::ParallelInnerNestedLoop(int Nx, int *d_Ny)
+{
+  int h_Ny[Nx];
+  gpuErrchk(cudaMemcpy(h_Ny, d_Ny, Nx*sizeof(int),
+		       cudaMemcpyDeviceToHost));
+  for (int ix=0; ix<Nx; ix++) {
+    int Ny = h_Ny[ix];
+    ParallelInnerNestedLoopKernel<i_func><<<(Ny+1023)/1024, 1024>>>(ix, Ny);
+    // gpuErrchk(cudaPeekAtLastError()); // uncomment only for debugging
+    // gpuErrchk(cudaDeviceSynchronize()); // uncomment only for debugging
+  }
+  gpuErrchk(cudaPeekAtLastError());
+  gpuErrchk(cudaDeviceSynchronize());
+
+  return 0;
+}
+
+//////////////////////////////////////////////////////////////////////
+template<int i_func>
+int NestedLoop::ParallelOuterNestedLoop(int Nx, int *d_Ny)
+{
+  ParallelOuterNestedLoopKernel<i_func><<<(Nx+1023)/1024, 1024>>>(Nx, d_Ny);
+  gpuErrchk(cudaPeekAtLastError());
+  gpuErrchk(cudaDeviceSynchronize());
+  
+  return 0;
+}
+
+//////////////////////////////////////////////////////////////////////
+template<int i_func>
+int NestedLoop::Frame1DNestedLoop(int Nx, int *d_Ny)
+{
+  if (Nx <= 0) return 0;
+  int dim_x, dim_y;
+
+  // Run sorting operation
+  cub::DeviceRadixSort::SortPairs(d_sort_storage_, sort_storage_bytes_,
+				  d_Ny, d_sorted_Ny_, d_idx_, d_sorted_idx_,
+				  Nx);
+  
+  int ix0 = Nx;
+  while(ix0>0) {
+    gpuErrchk(cudaMemcpy(&dim_y, &d_sorted_Ny_[ix0-1], sizeof(int),
+			    cudaMemcpyDeviceToHost));
+    if (dim_y < 1) dim_y = 1;
+    dim_x = (frame_area_ - 1) / dim_y + 1;
+    ix0 -= dim_x;
+    if (ix0<0) {
+      dim_x += ix0;
+      ix0 = 0;
+    } 
+    Frame1DNestedLoopKernel<i_func><<<(dim_x*dim_y+1023)/1024, 1024>>>
+      (ix0, dim_x, dim_y, d_sorted_idx_, d_sorted_Ny_);
+  }
+  gpuErrchk(cudaPeekAtLastError());
+  gpuErrchk(cudaDeviceSynchronize());
+  
+  return 0;
+}
+
+//////////////////////////////////////////////////////////////////////
+template<int i_func>
+int NestedLoop::Frame2DNestedLoop(int Nx, int *d_Ny)
+{
+  if (Nx <= 0) return 0;
+  // Sort the pairs (ix, Ny) with ix=0,..,Nx-1 in ascending order of Ny.
+  // After the sorting operation, d_sorted_idx_ are the reordered indexes ix
+  // and d_sorted_Ny_ are the sorted values of Ny 
+  cub::DeviceRadixSort::SortPairs(d_sort_storage_, sort_storage_bytes_,
+				  d_Ny, d_sorted_Ny_, d_idx_, d_sorted_idx_,
+				  Nx);  
+  int ix0 = Nx;	      // proceeds from right to left
+  while(ix0>0) {
+    int dim_x, dim_y;  // width and height of the rectangular frame
+    gpuErrchk(cudaMemcpy(&dim_y, &d_sorted_Ny_[ix0-1], sizeof(int),
+			    cudaMemcpyDeviceToHost));
+    if (dim_y < 1) dim_y = 1;
+    // frame_area_ is the fixed value of the the rectangular frame area
+    dim_x = (frame_area_ - 1) / dim_y + 1; // width of the rectangular frame
+    ix0 -= dim_x; // update the index value
+    if (ix0<0) {
+      dim_x += ix0;  // adjust the width if ix0<0 
+      ix0 = 0;
+    }    
+    dim3 threadsPerBlock(block_dim_x_, block_dim_y_);  // block size
+    dim3 numBlocks((dim_x - 1)/threadsPerBlock.x + 1,
+		   (dim_y - 1)/threadsPerBlock.y + 1);
+    // run a nested loop kernel on the rectangular frame
+    Frame2DNestedLoopKernel<i_func> <<<numBlocks,threadsPerBlock>>>
+      (ix0, dim_x, dim_y, d_sorted_idx_, d_sorted_Ny_);
+
+  }
+  gpuErrchk(cudaPeekAtLastError());
+  gpuErrchk(cudaDeviceSynchronize());
+
+  return 0;
+}
+
+//////////////////////////////////////////////////////////////////////
+template<int i_func>
+int NestedLoop::Smart1DNestedLoop(int Nx, int *d_Ny)
+{
+  // Find max value of Ny
+  cub::DeviceReduce::Max(d_reduce_storage_, reduce_storage_bytes_, d_Ny,
+			 d_max_Ny_, Nx);
+  int max_Ny;
+  gpuErrchk(cudaMemcpy(&max_Ny, d_max_Ny_, sizeof(int),
+			  cudaMemcpyDeviceToHost));
+  if (Nx <= 0) return 0;
+  float f_Nx = 2.0*log((float)Nx)-5;
+  int i_Nx = (int)floor(f_Nx);
+  int Ny_th;
+  if (i_Nx<0) {
+    Ny_th = Ny_th_arr_[0];
+  }
+  else if (i_Nx>=Ny_arr_size_-1) {
+    Ny_th = Ny_th_arr_[Ny_arr_size_-1];
+  }
+  else {
+    float t = f_Nx - (float)i_Nx;
+    Ny_th = Ny_th_arr_[i_Nx]*(1.0 - t) + Ny_th_arr_[i_Nx+1]*t;
+  }
+  if (max_Ny<Ny_th) {
+    return SimpleNestedLoop<i_func>(Nx, d_Ny, max_Ny);
+  }
+
+  if(max_Ny < 1) max_Ny = 1;
+  
+  int dim_x, dim_y;
+
+  // Run sorting operation
+  cub::DeviceRadixSort::SortPairs(d_sort_storage_, sort_storage_bytes_,
+				  d_Ny, d_sorted_Ny_, d_idx_, d_sorted_idx_,
+				  Nx);
+  // CudaCheckError(); // uncomment only for debugging
+  
+  int ix1 = (int)round(x_lim_*Nx);
+  if (ix1==Nx) ix1 = Nx - 1;
+  int Ny1;
+  gpuErrchk(cudaMemcpy(&Ny1, &d_sorted_Ny_[ix1], sizeof(int),
+			  cudaMemcpyDeviceToHost));
+  if(Ny1 < 1) Ny1 = 1;
+
+  dim3 threadsPerBlock(block_dim_x_, block_dim_y_);  // block size
+  int nbx = (Nx - 1)/threadsPerBlock.x + 1;
+  int nby = (Ny1 - 1)/threadsPerBlock.y + 1;
+  Ny1 = nby*threadsPerBlock.y;
+  
+  dim3 numBlocks(nbx, nby);
+  SimpleNestedLoopKernel<i_func> <<<numBlocks,threadsPerBlock>>>(Nx, d_Ny);
+  //CudaCheckError(); // uncomment only for debugging
+  
+  int ix0 = Nx;
+  while(ix0>ix1) {
+    gpuErrchk(cudaMemcpy(&dim_y, &d_sorted_Ny_[ix0-1], sizeof(int),
+			    cudaMemcpyDeviceToHost));
+    dim_y -= Ny1;
+    if (dim_y<=0) break;
+    dim_x = (frame_area_ - 1) / dim_y + 1;
+    ix0 -= dim_x;
+    if (ix0<ix1) {
+      dim_x += ix0 - ix1;
+      ix0 = ix1;
+    } 
+    Smart1DNestedLoopKernel<i_func><<<(dim_x*dim_y+1023)/1024, 1024>>>
+      (ix0, Ny1, dim_x, dim_y, d_sorted_idx_, d_sorted_Ny_);
+    //CudaCheckError(); // uncomment only for debugging
+  }
+  gpuErrchk(cudaPeekAtLastError());
+  gpuErrchk(cudaDeviceSynchronize());
+
+  return 0;
+}
+
+//////////////////////////////////////////////////////////////////////
+template<int i_func>
+int NestedLoop::Smart2DNestedLoop(int Nx, int *d_Ny)
+{
+  // Find max value of Ny
+  cub::DeviceReduce::Max(d_reduce_storage_, reduce_storage_bytes_, d_Ny,
+			 d_max_Ny_, Nx);
+  int max_Ny;
+  gpuErrchk(cudaMemcpy(&max_Ny, d_max_Ny_, sizeof(int),
+			  cudaMemcpyDeviceToHost));
+  if (Nx <= 0) return 0;
+  float f_Nx = 2.0*log((float)Nx)-5;
+  int i_Nx = (int)floor(f_Nx);
+  int Ny_th;
+  if (i_Nx<0) {
+    Ny_th = Ny_th_arr_[0];
+  }
+  else if (i_Nx>=Ny_arr_size_-1) {
+    Ny_th = Ny_th_arr_[Ny_arr_size_-1];
+  }
+  else {
+    float t = f_Nx - (float)i_Nx;
+    Ny_th = Ny_th_arr_[i_Nx]*(1.0 - t) + Ny_th_arr_[i_Nx+1]*t;
+  }
+  if (max_Ny<Ny_th) {
+    return SimpleNestedLoop<i_func>(Nx, d_Ny, max_Ny);
+  }
+
+  if(max_Ny < 1) max_Ny = 1;
+
+  int dim_x, dim_y;
+
+  // Run sorting operation
+  cub::DeviceRadixSort::SortPairs(d_sort_storage_, sort_storage_bytes_,
+				  d_Ny, d_sorted_Ny_, d_idx_, d_sorted_idx_,
+				  Nx);
+  // CudaCheckError(); // uncomment only for debugging
+  
+  int ix1 = (int)round(x_lim_*Nx);
+  if (ix1==Nx) ix1 = Nx - 1;
+  int Ny1;
+  gpuErrchk(cudaMemcpy(&Ny1, &d_sorted_Ny_[ix1], sizeof(int),
+			  cudaMemcpyDeviceToHost));
+  if(Ny1 < 1) Ny1 = 1;
+
+  dim3 threadsPerBlock(block_dim_x_, block_dim_y_);  // block size
+  int nbx = (Nx - 1)/threadsPerBlock.x + 1;
+  int nby = (Ny1 - 1)/threadsPerBlock.y + 1;
+  Ny1 = nby*threadsPerBlock.y;
+  
+  dim3 numBlocks(nbx, nby);
+  SimpleNestedLoopKernel<i_func> <<<numBlocks,threadsPerBlock>>>(Nx, d_Ny);
+  //CudaCheckError(); // uncomment only for debugging
+  
+  int ix0 = Nx;
+  while(ix0>ix1) {
+    gpuErrchk(cudaMemcpy(&dim_y, &d_sorted_Ny_[ix0-1], sizeof(int),
+			    cudaMemcpyDeviceToHost));
+    dim_y -= Ny1;
+    if (dim_y<=0) break;
+    dim_x = (frame_area_ - 1) / dim_y + 1;
+    ix0 -= dim_x;
+    if (ix0<ix1) {
+      dim_x += ix0 - ix1;
+      ix0 = ix1;
+    }
+
+    dim3 threadsPerBlock(block_dim_x_, block_dim_y_);  // block size
+    dim3 numBlocks((dim_x - 1)/threadsPerBlock.x + 1,
+		   (dim_y - 1)/threadsPerBlock.y + 1);
+    Smart2DNestedLoopKernel<i_func> <<<numBlocks,threadsPerBlock>>>
+      (ix0, Ny1, dim_x, dim_y, d_sorted_idx_, d_sorted_Ny_);
+    //CudaCheckError(); // uncomment only for debugging      
+  }
+  gpuErrchk(cudaPeekAtLastError());
+  gpuErrchk(cudaDeviceSynchronize());
+  
+  return 0;
+}
 
 //////////////////////////////////////////////////////////////////////
 template<int i_func>
