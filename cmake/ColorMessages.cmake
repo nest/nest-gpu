ab29b80f84e992b12475e6db7b16b6f684225cec
Jose Villamar <j.villamar@fz-juelich.de>
2022-11-18 21:25:06 +0100

    Updated license statements

--- cmake/ColorMessages.cmake
+++ cmake/ColorMessages.cmake
@@ -1,21 +1,21 @@
 # ColorMessages.cmake
 #
-# This file is part of NEST.
+# This file is part of NEST GPU.
 #
 # Copyright (C) 2004 The NEST Initiative
 #
-# NEST is free software: you can redistribute it and/or modify
+# NEST GPU is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 #
-# NEST is distributed in the hope that it will be useful,
+# NEST GPU is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+# along with NEST GPU.  If not, see <http://www.gnu.org/licenses/>.
 
 # This module defines functions to print warnings, errors and status with colors
 
