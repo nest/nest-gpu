61f5b395f325f9e280042ac9fb96374320067eb7
Gianmarco <gianmarcotiddia@gmail.com>
2022-12-05 20:10:05 +0100

    updated headers

--- python/Potjans_2014_hc/stimulus_params.py
+++ python/Potjans_2014_hc/stimulus_params.py
@@ -1,24 +1,26 @@
-# -*- coding: utf-8 -*-
 #
-# stimulus_params.py
+#  stimulus_params.py
 #
-# This file is part of NEST.
+#  This file is part of NEST GPU.
 #
-# Copyright (C) 2004 The NEST Initiative
+#  Copyright (C) 2021 The NEST Initiative
 #
-# NEST is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
+#  NEST GPU is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
 #
-# NEST is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#  NEST GPU is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
-
+#  You should have received a copy of the GNU General Public License
+#  along with NEST GPU. If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#
+#
 """ PyNEST Microcircuit: Stimulus Parameters
 -----------------------------------------------
 
