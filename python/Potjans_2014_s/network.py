5cbf2de783dc6f36970b86ed3098c17892c3bca2
Gianmarco <gianmarcotiddia@gmail.com>
2022-12-05 20:21:56 +0100

    adapted header to the NEST GPU standard

--- python/Potjans_2014_s/network.py
+++ python/Potjans_2014_s/network.py
@@ -1,22 +1,23 @@
-# -*- coding: utf-8 -*-
 #
-# network.py
 #
-# adapted for NESTGPU by Bruno Golosio
-# Copyright (C) 2004 The NEST Initiative
+#  network.py
 #
-# NEST is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
+#  This file is part of NEST GPU.
 #
-# NEST is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#  Copyright (C) 2021 The NEST Initiative
 #
-# You should have received a copy of the GNU General Public License
-# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+#  NEST is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  NEST is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with NEST.  If not, see <http://www.gnu.org/licenses/>.
 
 """ Microcircuit: Network Class
 ----------------------------------------
