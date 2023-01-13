91b91c0d82856672082a3e9fbce14cd64b080b9e
Jose Villamar <j.villamar@fz-juelich.de>
2023-01-13 07:48:32 +0100

    Adapted CMake branch to new 2.0-dev branch

---
+++ cmake/FindLTDL.cmake
@@ -0,0 +1,73 @@
+# FindLTDL.cmake
+#
+# This file is part of NEST GPU.
+#
+# Copyright (C) 2004 The NEST Initiative
+#
+# NEST GPU is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# NEST GPU is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST GPU.  If not, see <http://www.gnu.org/licenses/>.
+
+# - Find ltdl header and library
+#
+# This module defines
+#  LTDL_FOUND, if false, do not try to use LTDL.
+#  LTDL_INCLUDE_DIRS, where to find ltdl.h.
+#  LTDL_LIBRARIES, the libraries to link against to use libltdl.
+#  LTDL_VERSION, the library version
+#  LIBTOOL_EXECUTABLE, executable that provide generalized library-building support services.
+#
+# As a hint allows LTDL_ROOT_DIR
+
+find_path( LTDL_INCLUDE_DIR
+    NAMES ltdl.h
+    HINTS ${LTDL_ROOT_DIR}/include
+    )
+find_library( LTDL_LIBRARY
+    NAMES ltdl
+    HINTS ${LTDL_ROOT_DIR}/lib
+    )
+find_program( LIBTOOL_EXECUTABLE
+    NAMES glibtool libtool libtoolize
+    HINTS ${LTDL_ROOT_DIR}/bin
+    )
+
+if ( NOT LIBTOOL_EXECUTABLE STREQUAL "LIBTOOL_EXECUTABLE-NOTFOUND" )
+  execute_process(
+      COMMAND ${LIBTOOL_EXECUTABLE} --version
+      RESULT_VARIABLE RESULT
+      OUTPUT_VARIABLE LTDL_VAR_OUTPUT
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  if ( RESULT EQUAL 0 )
+    string( REGEX REPLACE ".* ([0-9]+\\.[0-9]+\\.[0-9]+).*" "\\1"
+                          LTDL_VERSION ${LTDL_VAR_OUTPUT} )
+  endif ()
+endif ()
+
+include( FindPackageHandleStandardArgs )
+find_package_handle_standard_args( LTDL
+  FOUND_VAR
+    LTDL_FOUND
+  REQUIRED_VARS
+    LTDL_LIBRARY
+    LTDL_INCLUDE_DIR
+  VERSION_VAR
+    LTDL_VERSION
+    )
+
+if ( LTDL_FOUND )
+  set( LTDL_INCLUDE_DIRS "${LTDL_INCLUDE_DIR}" )
+  set( LTDL_LIBRARIES "${LTDL_LIBRARY}" )
+endif ()
+
+mark_as_advanced( LTDL_ROOT_DIR LTDL_INCLUDE_DIR LTDL_LIBRARY LIBTOOL_EXECUTABLE )
